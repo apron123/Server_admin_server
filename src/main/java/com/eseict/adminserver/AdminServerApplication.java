34775d03df874caeba105addc327094e2247fafc
zium_sm <apronsksk@ziumks.com>
2024-10-14 11:22:33 +0900

    first commit

---
+++ src/main/java/com/eseict/adminserver/AdminServerApplication.java
@@ -0,0 +1,17 @@
+package com.eseict.adminserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+@CrossOrigin("*")
+@SpringBootApplication
+@EnableAdminServer
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/eseict/adminserver/config/RestTemplateInterceptor.java
@@ -0,0 +1,8 @@
+package com.eseict.adminserver.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RestTemplateInterceptor {
+
+}
